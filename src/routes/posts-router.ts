951bf41b3110316748c9ffbd7cd9f23fddaf33d1
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-05-02 01:08:01 +0300

    fix validate body

--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -2,8 +2,9 @@ import {Router} from "express";
 import {basicAuthMiddleware} from "../middlewares/basic-auth";
 import {createPost, deletePost, getAllPosts, getPost, updatePost} from "../controllers/posts.controller";
 import {postValidateSchema} from "../Schemes/posts-schema";
+import {inputValidationMiddleware} from "../middlewares/input-validation";
 
 export const postsRouter = Router();
 
-postsRouter.route('/').get(getAllPosts).post(basicAuthMiddleware, postValidateSchema, createPost);
-postsRouter.route('/:postId').get(getPost).put(basicAuthMiddleware, postValidateSchema, updatePost).delete(basicAuthMiddleware, deletePost);
+postsRouter.route('/').get(getAllPosts).post(basicAuthMiddleware, postValidateSchema, inputValidationMiddleware, createPost);
+postsRouter.route('/:postId').get(getPost).put(basicAuthMiddleware, postValidateSchema, inputValidationMiddleware, updatePost).delete(basicAuthMiddleware, deletePost);
