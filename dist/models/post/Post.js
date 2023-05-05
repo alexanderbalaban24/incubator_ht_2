b0ea08de2faa95fa577eb0c578f71ef3e891549b
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-05-06 01:35:18 +0300

    add createdAt

--- dist/models/post/Post.js
+++ dist/models/post/Post.js
@@ -2,12 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Post = void 0;
 class Post {
-    constructor(title, shortDescription, content, blogId, blogName) {
+    constructor(title, shortDescription, content, blogId, blogName, createdAt = new Date().toISOString()) {
         this.title = title;
         this.shortDescription = shortDescription;
         this.content = content;
         this.blogId = blogId;
         this.blogName = blogName;
+        this.createdAt = createdAt;
         this.id = new Date().toISOString();
     }
 }
--- src/models/post/Post.ts
+++ src/models/post/Post.ts
@@ -10,6 +10,7 @@ export class Post implements PostType {
         public shortDescription: string,
         public content: string,
         public blogId: string,
-        public blogName: string
+        public blogName: string,
+        public createdAt: string = new Date().toISOString()
     ){}
 }
