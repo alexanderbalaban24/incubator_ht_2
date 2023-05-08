e91e815f68abbdfde61dfa72b2d1b8ca9c40443b
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-05-08 20:44:42 +0300

    hot fix

--- src/middlewares/basic-auth.ts
+++ src/middlewares/basic-auth.ts
@@ -4,7 +4,7 @@ import {btoa} from "buffer";
 export const basicAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const type = req.header("Authorization")?.split(" ")[0].trim();
     const token = req.header("Authorization")?.split(" ")[1].trim();
-    const tokenOrigin = Buffer.from(`${process.env.LOGIN || "admin"}:${process.env.PASS || "qwerty"}`).toString("base64");
+    const tokenOrigin = btoa(`${process.env.LOGIN || "admin"}:${process.env.PASS || "qwerty"}`);
     if (token === tokenOrigin && type === "Basic") {
         next();
     } else {
