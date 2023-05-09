5eb76dc7424ac82ad9872a3a014f77a908f01dd9
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-05-09 17:58:50 +0300

    refactor testing endpoint

--- src/repositories/testing/testing-command-repository.ts
+++ src/repositories/testing/testing-command-repository.ts
@@ -1,11 +1,13 @@
 import {blogsCollections} from "../../db/collections/blogsCollections";
 import {postsCollections} from "../../db/collections/postsCollections";
+import {usersCollections} from "../../db/collections/usersCollections";
 
 export const testingCommandRepository = {
     async deleteAllDB() {
         const resDeletedBlogs = await blogsCollections.deleteMany({});
         const resDeletedPosts = await postsCollections.deleteMany({});
+        const resDeletedUsers = await usersCollections.deleteMany({});
 
-        return resDeletedBlogs.deletedCount === 1 && resDeletedPosts.deletedCount === 1;
+        return resDeletedBlogs.deletedCount === 1 && resDeletedPosts.deletedCount === 1 && resDeletedUsers.deletedCount === 1;
     }
 }
