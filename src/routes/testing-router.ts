1843057d87fbb7d7410e70d33493a079e8c76069
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-05-10 22:41:50 +0300

    add env in vercel

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -3,4 +3,4 @@ import {deleteAllData} from "../controllers/testing.controller";
 
 export const testingRouter = Router();
 
-testingRouter.route('/all-data').delete(deleteAllData)
+testingRouter.route('/all-data').delete(deleteAllData);
