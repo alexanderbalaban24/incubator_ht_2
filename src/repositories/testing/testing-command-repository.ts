7cb20d654a95127700b9686133e248abc6f6b86e
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-05-10 21:58:53 +0300

    hot fix

--- src/repositories/testing/testing-command-repository.ts
+++ src/repositories/testing/testing-command-repository.ts
@@ -1,13 +1,15 @@
 import {blogsCollections} from "../../db/collections/blogsCollections";
 import {postsCollections} from "../../db/collections/postsCollections";
 import {usersCollections} from "../../db/collections/usersCollections";
+import {commentsCollections} from "../../db/collections/commentsCollections";
 
 export const testingCommandRepository = {
     async deleteAllDB() {
         const resDeletedBlogs = await blogsCollections.deleteMany({});
         const resDeletedPosts = await postsCollections.deleteMany({});
         const resDeletedUsers = await usersCollections.deleteMany({});
+        const resDeletedComments = await commentsCollections.deleteMany({});
 
-        return resDeletedBlogs.deletedCount === 1 && resDeletedPosts.deletedCount === 1 && resDeletedUsers.deletedCount === 1;
+        return resDeletedBlogs.deletedCount === 1 && resDeletedPosts.deletedCount === 1 && resDeletedUsers.deletedCount === 1 && resDeletedComments.deletedCount === 1;
     }
 }
